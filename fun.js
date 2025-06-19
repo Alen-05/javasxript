f9d7164e3d6b7d818465ce764058ca10b973c38e
Alen-05 <abhp0706@gmail.com>
2025-06-19 15:42:54 +0530

    changed

---
+++ fun.js
@@ -0,0 +1,54 @@
+function hello() {
+  console.log("Hello");
+}
+hello();
+function add(a, b) {
+  console.log(a + b);
+}
+add(1, 2);
+
+function displayAnotherFunction(func) {
+  func(1, 2);
+}
+
+displayAnotherFunction(add);
+
+let varFunc = add;
+varFunc(1, 4);
+
+//Anonymous Function inside a variable
+let newFuncVar = function () {
+  console.log("Variable Function");
+};
+newFuncVar();
+
+const arrowFuncVar = () => {
+  console.log("Arrow Functions");
+};
+
+arrowFuncVar();
+
+//Filter, Map, Reduce
+let nums = [1, 2, 3, 4, 5];
+
+let evens = nums.filter((n) => {
+  return n % 2 === 0;
+});
+
+let newEvens = nums.filter((n) => n % 2 === 0);
+console.log(evens);
+
+let newList = [];
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 0) {
+    newList.push(nums[i]);
+  }
+}
+console.log(newList);
+
+// Map method
+console.log("Using Map methos");
+let squares = nums.map((n) => {
+  return n * n;
+});
+console.log(squares);
